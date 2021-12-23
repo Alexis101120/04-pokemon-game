72514878a33af832da06694ce987625958b7a885
Alexis101120 <65004075+Alexis101120@users.noreply.github.com>
2021-12-22 22:47:59 -0700

    Inicio proyecto

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>¿Quien es ese pokemon?</h1>
+      <Picture></Picture>
+      <Options></Options>
+  </div>
+</template>
+
+
+<script>
+import Picture from '@/components/PokemonPicture.vue'
+import Options from '@/components/PokemonOptions.vue'
+export default {
+    components: {
+    Picture,
+    Options
+  }
+}
+</script>
